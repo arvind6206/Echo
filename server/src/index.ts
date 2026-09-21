349b52747cd39e014d8672de81a666cd522788ad
arvind6206 <vishwkarmaarvind1222@gmail.com>
2026-09-21 14:42:43 +0530

    Latest

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,18 @@
 import { WebSocketServer } from 'ws'
 
 const wss = new WebSocketServer({port: 8080})
+
+let userCount = 0;
+
+wss.on('connection', (socket) => {
+    userCount += 1;
+    console.log("user connected #" + userCount)
+
+    socket.on('message', (message) => {
+        console.log("message received " + message.toString())
+        socket.send(message.toString() + ": sent from the server")
+    })
+})
+
+
+
