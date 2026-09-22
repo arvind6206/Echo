c9ee602c9e50a3b4531c973b72f8e3321521b009
arvind6206 <vishwkarmaarvind1222@gmail.com>
2026-09-22 07:56:22 +0530

    broadcasting logic

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,21 @@
-import { WebSocketServer } from 'ws'
+import { WebSocketServer, WebSocket } from "ws";
 
-const wss = new WebSocketServer({port: 8080})
+const wss = new WebSocketServer({ port: 8080 });
 
 let userCount = 0;
+let allSockets: WebSocket[] = [];
 
-wss.on('connection', (socket) => {
-    userCount += 1;
-    console.log("user connected #" + userCount)
+wss.on("connection", (socket) => {
+  allSockets.push(socket);
 
-    socket.on('message', (message) => {
-        console.log("message received " + message.toString())
-        socket.send(message.toString() + ": sent from the server")
-    })
-})
+  userCount += 1;
+  console.log("user connected #" + userCount);
 
-
-
+  socket.on("message", (message) => {
+    console.log("message received " + message.toString());
+    for (let i = 0; i < allSockets.length; i++) {
+      const s = allSockets[i]!;
+      s.send(message.toString() + ": sent from the server");
+    }
+  });
+});
